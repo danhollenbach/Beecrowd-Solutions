9da0ac5420ce6c1f871541b4492ae0687e3cffab
dan.hollenbach <daniel.f.edens@gmail.com>
2024-08-20 11:17:24 -0300

    ✨ feat: 2520 solution

---
+++ The Last Analogimon - 2520.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void matrixFill(int n, int m, int matriz[n][m]){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+}
+
+void achaSabre(int n, int m, int matriz[n][m]){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(matriz[i][j] == 42){
+                if(i > 0 && i < n - 1 && j > 0 && j < m - 1){
+                    if(matriz[i - 1][j] == 7 && matriz[i + 1][j] == 7 && matriz[i][j - 1] == 7 && matriz[i][j + 1] == 7 && matriz[i - 1][j - 1] == 7 && matriz[i - 1][j + 1] == 7 && matriz[i + 1][j - 1] == 7 && matriz[i + 1][j + 1] == 7){
+                        printf("%d %d\n", i + 1, j + 1);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    printf("0 0\n");
+}
+
+int main(){
+    int n,m;
+
+    scanf("%d %d", &n, &m);
+
+    int matriz[n][m];
+
+    matrixFill(n, m, matriz);
+
+    achaSabre(n, m, matriz);
+
+    return 0;
+}
