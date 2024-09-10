f0749ef78202441086a52e7aa8dd9bd0cdae43c9
pocoyo-da-krebrada <164937314+pocoyo-da-krebrada@users.noreply.github.com>
2024-09-10 08:36:30 -0300

    ✨feat: 1305 solution

---
+++ Rounder - 1305.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define true 1
+#define false 0
+
+void main()
+{
+
+    char num[100];
+    char cutoff[100];
+
+    while (scanf("%s %s", num, cutoff) != EOF)
+    {
+
+        int inteiro = atoi(num);
+        double frac = atof(num) - inteiro;
+        double _cutoff = atof(cutoff);
+
+        if (frac > _cutoff)
+            inteiro++;
+
+        printf("%d\n", inteiro);
+    }
+}
