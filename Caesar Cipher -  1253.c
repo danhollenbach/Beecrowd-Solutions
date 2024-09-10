ae1fcdf44127b1cac71aef60300663998569e66b
pocoyo-da-krebrada <164937314+pocoyo-da-krebrada@users.noreply.github.com>
2024-09-10 08:16:14 -0300

    ✨feat: 1253 solution

---
+++ Caesar Cipher -  1253.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    unsigned short testes, deslocamento;
+    unsigned short i, j;
+
+    scanf("%hd", &testes);
+
+    for (i = 0; i < testes; i++)
+    {
+        char palavra[50], letra;
+        scanf("%s", palavra);
+        scanf("%hd", &deslocamento);
+
+        for (j = 0; j < strlen(palavra); j++)
+        {
+            if (palavra[j] - deslocamento < 'A')
+            {
+                letra = '[' - (deslocamento - (palavra[j] - 'A'));
+                printf("%c", letra);
+            }
+            else
+            {
+                letra = palavra[j] - deslocamento;
+                printf("%c", letra);
+            }
+        }
+        printf("\n");
+    }
+}
